4127601e9f599edbc664fff02615ded2f6bd2c7d
babahanX <bbekirbatuhan@gmail.com>
2019-04-15 16:33:02 +0300

    AudioEngine_FMOD refactor.

--- LinaEngine/src/PackageManager/Audio/AudioEngine_FMOD.cpp
+++ LinaEngine/src/PackageManager/Audio/AudioEngine_FMOD.cpp
@@ -22,101 +22,169 @@ namespace LinaEngine
 	{
 		LINA_CORE_TRACE("[Destructor] -> Audio Engine FMOD ({0})", typeid(*this).name());
 
+		// Release the sounds in each category.
 		SoundMap::iterator iter;
-		for (iter = m_Sounds.begin(); iter != m_Sounds.end(); iter++)
+		for (int i = 0; i < CATEGORY_COUNT; ++i)
 		{
-			iter->second->release();
+			for (iter = m_Sounds[i].begin(); iter != m_Sounds[i].end(); ++iter)
+			{
+				iter->second->release();
+			}
+
+			m_Sounds[i].clear();
 		}
 
-		m_Sounds.clear();
-
+		// Release the system.
 		m_System->release();
-		m_System = nullptr;
 	}
 
 	void LinaEngine::AudioEngine_FMOD::Initialize()
 	{
 		AudioEngine::Initialize();
 
-		LINA_CORE_TRACE("[Initialization] -> Audio Engine FMOD ({0})", typeid(*this).name());
-
 		FMOD::System_Create(&m_System);
 		m_System->init(100, FMOD_INIT_NORMAL, 0);
+
+		// Create channels groups for each Category
+		m_System->getMasterChannelGroup(&m_Master);
+		for (int i = 0; i < CATEGORY_COUNT; ++i)
+		{
+			m_System->createChannelGroup(0, &m_Groups[i]);
+			m_Master->addGroup(m_Groups[i]);
+		}
+
+		// Set up modes for each Category
+		m_Modes[CATEGORY_SFX] = FMOD_DEFAULT;
+		m_Modes[CATEGORY_SONG] = FMOD_DEFAULT | FMOD_CREATESTREAM | FMOD_LOOP_NORMAL;
+
+		// Seed the random number generator for SFXs.
+		srand(time(0));
+
+		LINA_CORE_TRACE("[Initialization] -> Audio Engine FMOD ({0})", typeid(*this).name());
 	}
 
 	void LinaEngine::AudioEngine_FMOD::Start()
 	{
-		Load(ResourceConstants::SoundsPath + "jaguar.wav");
+		LoadSFX(ResourceConstants::SoundsPath + "jaguar.wav");
 	}
 
 	void LinaEngine::AudioEngine_FMOD::OnUpdate()
 	{
-		Update();
 
 		OnInput(app->GetInputEngine());
 	}
 
 	void LinaEngine::AudioEngine_FMOD::OnEvent(Event & e)
 	{
-
 	}
 
 	void AudioEngine_FMOD::OnInput(InputEngine& i)
 	{
-		if (i.GetKey(LINA_KEY_B))
+		if (i.GetKeyDown(LINA_KEY_B))
 		{
-			Play(ResourceConstants::SoundsPath + "jaguar.wav");
+			PlaySFX(ResourceConstants::SoundsPath + "jaguar.wav", 0.0f, 1.0f, 20.0f, 2000.0f);
+		}
+		if (i.GetKeyDown(LINA_KEY_V))
+		{
+			StopSFXs();
 		}
 	}
 
-	void AudioEngine_FMOD::Load(const std::string & path)
+	void AudioEngine_FMOD::LoadSFX(const std::string & path)
 	{
-		LoadOrStream(path, false);
+		Load(CATEGORY_SFX, path);
 	}
 
-	void AudioEngine_FMOD::Stream(const std::string & path)
+	void AudioEngine_FMOD::LoadSong(const std::string & path)
 	{
-		LoadOrStream(path, true);
+		Load(CATEGORY_SONG, path);
 	}
 
-	void AudioEngine_FMOD::Play(const std::string & path)
+	void AudioEngine_FMOD::PlaySFX(const std::string & path, float minVolume, float maxVolume, float minPitch, float maxPitch)
 	{
-		// Search if there is a matching sound in the SoundMap
-		SoundMap::iterator sound = m_Sounds.find(path);
-
-		// Ignore the function call if there is no sound
-		if (sound == m_Sounds.end())
+		// Try to find sound effect and return if not found.
+		SoundMap::iterator sound = m_Sounds[CATEGORY_SFX].find(path);
+		if (sound == m_Sounds[CATEGORY_SFX].end())
 			return;
 
-		// Otherwise play the sound.
-		m_System->playSound(sound->second, NULL, false, 0);
+		// Calculate random volume and pitch in selected range.
+		float volume = RandomBetween(minVolume, maxVolume);
+		float pitch = RandomBetween(minPitch, maxPitch);
+
+		// Play the sound effect with these initial sound values.
+		FMOD::Channel* channel;
+		m_System->playSound(sound->second, NULL, true, &channel);
+		channel->setChannelGroup(m_Groups[CATEGORY_SFX]);
+		channel->setVolume(volume);
+		
+
+		// Set the pitch randomly based on Semitone later. 
+		//float frequency;
+		//channel->getFrequency(&frequency);
+		//channel->setFrequency(ChangeSemitone(frequency, pitch));
+		
+		// After playing the sound, set the channel paused.
+		channel->setPaused(false);
 	}
 
-	void AudioEngine_FMOD::Update()
+	void AudioEngine_FMOD::PlaySong(const std::string & path)
 	{
-		m_System->update();
 	}
 
-	void AudioEngine_FMOD::LoadOrStream(const std::string & path, bool stream)
+	void AudioEngine_FMOD::StopSFXs()
 	{
-		// Ignore the function call if the sound is already loaded.
-		if (m_Sounds.find(path) != m_Sounds.end())
+		// Stop all the sound effects.
+		m_Groups[CATEGORY_SFX]->stop();
+	}
+
+	void AudioEngine_FMOD::StopSongs()
+	{
+	}
+
+	void AudioEngine_FMOD::SetMasterVolume(float volume)
+	{
+	}
+
+	void AudioEngine_FMOD::SetSFXsVolume(float volume)
+	{
+	}
+
+	void AudioEngine_FMOD::SetSongsVolume(float volume)
+	{
+	}
+
+	void AudioEngine_FMOD::Load(Category type, const std::string & path)
+	{
+		// If the requested sound is already in the SoundMap then return.
+		if (m_Sounds[type].find(path) != m_Sounds[type].end())
 			return;
 
-		// Load or stream file into a sound object.
+		// Else create the sound and insert it to the SoundMap.
 		FMOD::Sound* sound;
-		if (stream)
-		{
-			m_System->createStream(path.c_str(), FMOD_DEFAULT, 0, &sound);
-		}
-		else
-		{
-			m_System->createSound(path.c_str(), FMOD_DEFAULT, 0, &sound);
-		}
-
-		m_Sounds.insert(std::make_pair(path, sound));
+		m_System->createSound(path.c_str(), m_Modes[type], 0, &sound);
+		m_Sounds[type].insert(std::make_pair(path, sound));
 	}
 
+	float AudioEngine_FMOD::RandomBetween(float min, float max)
+	{
+		if (min == max)
+			return min;
+
+		float n = (float)rand() / (float)RAND_MAX;
+		return min + n * (max - min);
+	}
+
+	float AudioEngine_FMOD::ChangeOctave(float frequency, float variation)
+	{
+		static float octave_ratio = 2.0f;
+		return frequency * pow(octave_ratio, variation);
+	}
+
+	float AudioEngine_FMOD::ChangeSemitone(float frequency, float variation)
+	{
+		static float semitone_ratio = pow(2.0f, 1.0f / 12.0f);
+		return frequency * pow(semitone_ratio, variation);
+	}
 }
 
 #endif
--- LinaEngine/src/PackageManager/Audio/AudioEngine_FMOD.hpp
+++ LinaEngine/src/PackageManager/Audio/AudioEngine_FMOD.hpp
@@ -11,8 +11,6 @@
 #include <map>
 #include <fmod.hpp>
 
-typedef std::map<std::string, FMOD::Sound*> SoundMap;
-
 namespace LinaEngine
 {
 	class AudioEngine_FMOD : public AudioEngine
@@ -22,25 +20,47 @@ namespace LinaEngine
 		~AudioEngine_FMOD();
 
 		void Initialize() override;
-
 		void Start() override;
-
 		void OnUpdate() override;
-
 		void OnEvent(class Event& e) override;
-
 		void OnInput(class InputEngine& i);
 
-		void Load(const std::string& path);
-		void Stream(const std::string& path);
-		void Play(const std::string& path);
-		void Update();
+
+
+		void LoadSFX(const std::string& path);
+		void LoadSong(const std::string& path);
+		void PlaySFX(const std::string& path, float minVolume, float maxVolume, float minPitch, float maxPitch);
+		void PlaySong(const std::string& path);
+		void StopSFXs();
+		void StopSongs();
+		void SetMasterVolume(float volume);
+		void SetSFXsVolume(float volume);
+		void SetSongsVolume(float volume);
 
 	private:
 
-		void LoadOrStream(const std::string& path, bool stream);
+		//Temporary. Write the these three functions to the Math later(or use the already implemented ones).
+		float RandomBetween(float min, float max);
+		// Octave is distance between two consecutive notes with the same name.
+		float ChangeOctave(float frequency, float variation); 
+		// Semitone is distance between any to adjacent notes.
+		float ChangeSemitone(float frequency, float variation);
+
+	private:
+
+		typedef std::map<std::string, FMOD::Sound*> SoundMap;
+		enum Category { CATEGORY_SFX, CATEGORY_SONG, CATEGORY_COUNT };
+		void Load(Category type, const std::string& path);
 		FMOD::System* m_System;
-		SoundMap m_Sounds;
+		FMOD::ChannelGroup* m_Master;
+		FMOD::ChannelGroup* m_Groups[CATEGORY_COUNT];
+		SoundMap m_Sounds[CATEGORY_COUNT];
+		FMOD_MODE m_Modes[CATEGORY_COUNT];
+		FMOD::Channel* m_CurrentSong;
+		std::string m_CurrentSongPath;
+		std::string m_NextSongPath;
+		enum FadeState { FADE_NONE, FADE_IN, FADE_OUT };
+		FadeState m_Fade;
 	};
 }
 
